813b7ef83527289865cbc5419a47c8cba0ef5141
Shivelle <shivelle@gmx.net>
2017-07-21 13:53:24 +0200

    finish theory / add test component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <test></test>
   </div>
 </template>
 
 <script>
+import test from './components/test'
 export default {
-  name: 'app'
+  name: 'app', 
+  components: {
+    test
+  }
 }
 </script>
 
---
+++ src/components/test.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="test">
+		<h1>Test</h1>
+	</div>
+	
+</template>
+
+<script>
+	export default {
+		name: 'Test'
+	}
+
+</script>
+
+<style scoped>
+
+</style>
